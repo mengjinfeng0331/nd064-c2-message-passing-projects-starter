deb3bb2f4d19b14e6b6de1abb2094d3e74517cd9
jmeng <jmeng@datto.com>
2021-10-23 14:57:17 -0400

    update person_api

--- modules/person_api/scripts/run_db_command.sh
+++ modules/person_api/scripts/run_db_command.sh
@@ -2,7 +2,7 @@
 
 # Set database configurations
 export CT_DB_USERNAME=ct_admin
-export CT_DB_NAME=geoconnections
+export CT_DB_NAME=person
 
 
 cat ./db/2020-08-15_init-db.sql | kubectl exec -i $1 -- bash -c "psql -U $CT_DB_USERNAME -d $CT_DB_NAME"
