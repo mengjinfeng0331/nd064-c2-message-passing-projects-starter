cfccea3774b9fa0b289e528c78e08f2d46ba7a5b
jmeng <jmeng@datto.com>
2021-10-24 22:14:30 -0400

    update db sql

--- modules/connection_api/db/2020-08-15_init-db.sql
+++ modules/connection_api/db/2020-08-15_init-db.sql
@@ -3,7 +3,6 @@ CREATE TABLE location (
     person_id INT NOT NULL,
     coordinate GEOMETRY NOT NULL,
     creation_time TIMESTAMP NOT NULL DEFAULT NOW(),
-    FOREIGN KEY (person_id) REFERENCES person(id)
 );
 CREATE INDEX coordinate_idx ON location (coordinate);
 CREATE INDEX creation_time_idx ON location (creation_time);
